0034796a29eac7a85bd604826d176fa6614db4f7
Daniel Falcon <Daniel.falcon@viaro.net>
2025-06-23 14:30:12 -0600

    add handling of exception

--- cypress/e2e/Extra/extraTest.cy.js
+++ cypress/e2e/Extra/extraTest.cy.js
@@ -1,13 +1,11 @@
-describe('Rick Astley YouTube Search', () => {
-  it('Searches and clicks the official "Never Gonna Give You Up" video', () => {
-    cy.visit('https://www.youtube.com');
+describe('Extra test', () => {
+  it('Opens Event calendar', () => {
+    cy.visit('https://growthzonedev.com/');
 
-    // Wait for the search input to appear and type the search term
-    cy.get('.ytSearchboxComponentInput').should('be.visible').type('never gonna give you up{enter}');
+    cy.get('.ribbon-content')
+      .should('be.visible')
+      .invoke('removeAttr', 'target')
+      .click()
 
-    // Wait for results to load and click the correct video
-    cy.get('ytd-video-renderer', { timeout: 10000 })
-      .contains('Rick Astley - Never Gonna Give You Up (Official Video)')
-      .click();
   });
 });
