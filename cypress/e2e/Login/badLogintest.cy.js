15ba9e9ed27022747e6f66df902a9d7616a0df86
Daniel Falcon <Daniel.falcon@viaro.net>
2025-06-23 14:34:09 -0600

    adding exception catch to all tests

--- cypress/e2e/Login/badLogintest.cy.js
+++ cypress/e2e/Login/badLogintest.cy.js
@@ -1,4 +1,9 @@
 describe('Growthzone Login', () => {
+  beforeEach(() => {
+    Cypress.on('uncaught:exception', (err, runnable) => {
+      return false; // Ignore for this suite
+    });
+  });
   it('Login with invalid credentials', () => {
     cy.visit('https://growthzonedev.com/');
     cy.get('#check-user-name-field')
